bbe7c3e97214420a048f9f7e6ca7cb9ab1a2cc1c
bryanmwaniqi <bryanmwaniqi@gmail.com>
2022-03-01 18:26:26 +0300

    add features owl carousel

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#features-slider').owlCarousel({
+        items: 1,
+        margin: 0,
+        loop: true,
+        autoplay: true,
+        dots: true,
+        autoplayHoverPause: true
+    });
+})
