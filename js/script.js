ced9f516239fbef756b5cf23f776f813b0a25cf0
bryanmwaniqi <bryanmwaniqi@gmail.com>
2022-04-02 10:36:43 +0300

    add mainbar navbar and update sidebar menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function () {
+    // Dashboard features carousel
     $('#features-slider').owlCarousel({
         items: 1,
         margin: 0,
@@ -7,4 +8,22 @@ $(function () {
         dots: true,
         autoplayHoverPause: true
     });
+
+    // sidemenu text toggler
+    let sideToggler = $('.toggle-input')
+    $('.sidemenu-toggle').on('click', function () {
+        if (sideToggler.is(':checked')) {
+            // $('.sidemenu li i').toggleClass('fa-lg')
+            $('#sidebar').toggleClass('menu-verbose')
+            $('.toggler-nav').toggleClass('active')
+            setTimeout(() => {
+                $('.menu-link').toggleClass('d-none')
+            }, 100);
+        }
+    })
+
+    // Mobile view sidemenu toggler
+    $('#sideMenuToggler').on('click', function () {
+        $('#sidebar').toggleClass('active')
+    })
 })
