dceb8268db316b2ce274ba621e010242e8fa8311
bryanmwaniqi <bryanmwaniqi@gmail.com>
2022-05-05 13:23:41 +0300

    add analytics banner and sales traffic classifiers divs

--- js/script.js
+++ js/script.js
@@ -49,6 +49,11 @@ $(function () {
         type: 'bar',
         data: data,
         options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
             scales: {
                 y: {
                     beginAtZero: true,
@@ -71,6 +76,138 @@ $(function () {
         config
     );
 
+    // Instagram bar chart
+    const igData = {
+        labels: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+        datasets: [{
+            data: [45, 59, 75, 65, 56, 55, 40, 55, 59, 70, 65, 56],
+            backgroundColor: '#5d39ff',
+            hoverBackgroundColor: '#ff0077',
+            borderWidth: 0,
+            borderRadius: 50,
+            barThickness: 6,
+            borderSkipped: false
+        }]
+    };
+    const igConfig = {
+        type: 'bar',
+        data: data,
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        display: false,
+                        // drawBorder: false
+                    }
+                },
+                x: {
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    }
+                }
+            }
+        },
+    };
+    const igChart = new Chart(
+        $('#igChart'),
+        igConfig
+    );
+
+    // facebook bar chart
+    const fbData = {
+        labels: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+        datasets: [{
+            data: [45, 59, 75, 65, 56, 55, 40, 55, 59, 70, 65, 56],
+            backgroundColor: '#5d39ff',
+            hoverBackgroundColor: '#ff0077',
+            borderWidth: 0,
+            borderRadius: 50,
+            barThickness: 6,
+            borderSkipped: false
+        }]
+    };
+    const fbConfig = {
+        type: 'bar',
+        data: data,
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        display: false,
+                        // drawBorder: false
+                    }
+                },
+                x: {
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    }
+                }
+            }
+        },
+    };
+    const fbChart = new Chart(
+        $('#fbChart'),
+        fbConfig
+    );
+
+    // Youtube bar chart
+    const ytData = {
+        labels: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+        datasets: [{
+            data: [45, 59, 75, 65, 56, 55, 40, 55, 59, 70, 65, 56],
+            backgroundColor: '#5d39ff',
+            hoverBackgroundColor: '#ff0077',
+            borderWidth: 0,
+            borderRadius: 50,
+            barThickness: 6,
+            borderSkipped: false
+        }]
+    };
+    const ytConfig = {
+        type: 'bar',
+        data: data,
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        display: false,
+                        // drawBorder: false
+                    }
+                },
+                x: {
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    }
+                }
+            }
+        },
+    };
+    const ytChart = new Chart(
+        $('#ytChart'),
+        ytConfig
+    );
+
     // best sellers horizontal bar chart
     const bestSellersData = {
         labels: ['electronics', 'cosmetics', 'fashion', 'food & drinks', 'art crafts', 'medical & health'],
@@ -89,6 +226,11 @@ $(function () {
         type: 'bar',
         data: bestSellersData,
         options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
             indexAxis: 'y',
             scales: {
                 y: {
