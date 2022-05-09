6916cd0d67bd6393f7cb2e3cc4bccee4744f6dee
bryanmwaniqi <bryanmwaniqi@gmail.com>
2022-05-09 13:19:54 +0300

    add target sales comparison chart,style stats divs

--- js/script.js
+++ js/script.js
@@ -298,4 +298,60 @@ $(function () {
         $('#bestSellersChart'),
         bestSellersConfig
     );
+
+     // Overall target vs sales bar chart
+    const targetData = {
+        labels: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+        datasets: [{
+            data: [45, 59, 81, 65, 56, 55, 40, 55, 59, 75, 65, 56],
+            label: 'Targeted sales',
+            backgroundColor: '#5d39ff',
+            hoverBackgroundColor: '#ff0077',
+            borderWidth: 0,
+            borderRadius: 50,
+            // barPercentage: 0.8,
+            // categoryPercentage: 0.5,
+            // barThickness: 12,
+            borderSkipped: false
+        }, {
+            data: [35, 45, 70, 50, 50, 35, 20, 40, 50, 70, 54, 48],
+            label: 'Actual Sales',
+            backgroundColor: 'rgba(93, 57, 255, 0.5)',
+            hoverBackgroundColor: '#ff0077',
+            borderWidth: 0,
+            borderRadius: 50,
+            // barPercentage: 0.8,
+            // categoryPercentage: 0.5,
+            // barThickness: 12,
+            borderSkipped: false
+        }]
+    };
+    const targetConfig = {
+        type: 'bar',
+        data: targetData,
+        options: {
+            categoryPercentage: 0.6,
+            barPercentage: 0.5,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        display: false,
+                        // drawBorder: false
+                    }
+                },
+                x: {
+                    barValueSpacing: 20,
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    }
+                }
+            }
+        },
+    };
+    const targetSalesChart = new Chart(
+        $('#targetSalesChart'),
+        targetConfig
+    );
 })
